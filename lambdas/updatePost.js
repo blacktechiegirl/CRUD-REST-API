0e98fb862eba04017954a54211e00e73a2c1e2dc
Aminat <amibarbs01@gmail.com>
2022-07-20 07:42:43 +0100

    updated update-post

--- lambdas/updatePost.js
+++ lambdas/updatePost.js
@@ -1,48 +1,60 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const db = new DynamoDBClient({});
-const {UpdateItemCommand} = require("@aws-sdk/client-dynamodb");
+const { UpdateItemCommand } = require("@aws-sdk/client-dynamodb");
 const { marshall } = require("@aws-sdk/util-dynamodb");
 
-
 const updatePost = async (event) => {
-    const response = { statusCode: 200 };
+  const response = { statusCode: 200 };
 
-    try {
-        const body = JSON.parse(event.body);
-        const objKeys = Object.keys(body);
-        const params = {
-            TableName: process.env.DYNAMODB_TABLE_NAME,
-            Key: marshall({
-                 postId: event.pathParameters.postId,
-                 userId: event.pathParameters.userId 
-                }),
-            UpdateExpression: `SET ${objKeys.map((_, index) => `#key${index} = :value${index}`).join(", ")}`,
-            ExpressionAttributeNames: objKeys.reduce((acc, key, index) => ({
-                ...acc,
-                [`#key${index}`]: key,
-            }), {}),
-            ExpressionAttributeValues: marshall(objKeys.reduce((acc, key, index) => ({
-                ...acc,
-                [`:value${index}`]: body[key],
-            }), {})),
-        };
-        const updateResult = await db.send(new UpdateItemCommand(params));
+  try {
+    const body = JSON.parse(event.body);
+    const objKeys = Object.keys(body);
 
-        response.body = JSON.stringify({
-            message: "Successfully updated post.",
-            updateResult,
-        });
-    } catch (e) {
-        console.error(e);
-        response.statusCode = 500;
-        response.body = JSON.stringify({
-            message: "Failed to update post.",
-            errorMsg: e.message,
-            errorStack: e.stack,
-        });
-    }
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Key: marshall({
+        postId: event.pathParameters.postId,
+        userId: event.pathParameters.userId,
+      }),
+      UpdateExpression: "SET content = :content",
+      ExpressionAttributeValues: marshall({
+        ":content": "I just got updated",
+      }),
+      ReturnValues: "ALL_NEW",
+    };
+    // const params = {
+    //     TableName: process.env.DYNAMODB_TABLE_NAME,
+    //     Key: marshall({
+    //          postId: event.pathParameters.postId,
+    //          userId: event.pathParameters.userId
+    //         }),
+    //     UpdateExpression: `SET ${objKeys.map((_, index) => `#key${index} = :value${index}`).join(", ")}`,
+    //     ExpressionAttributeNames: objKeys.reduce((acc, key, index) => ({
+    //         ...acc,
+    //         [`#key${index}`]: key,
+    //     }), {}),
+    //     ExpressionAttributeValues: marshall(objKeys.reduce((acc, key, index) => ({
+    //         ...acc,
+    //         [`:value${index}`]: body[key],
+    //     }), {})),
+    // };
+    const updateResult = await db.send(new UpdateItemCommand(params));
 
-    return response;
+    response.body = JSON.stringify({
+      message: "Successfully updated post.",
+      updateResult,
+    });
+  } catch (e) {
+    console.error(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: "Failed to update post.",
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
+  }
+
+  return response;
 };
 
-module.exports = { updatePost};
+module.exports = { updatePost };
