80ea4024ca8319e53cccd51babcfef4a799d73ac
Aminat <amibarbs01@gmail.com>
2022-08-11 20:37:17 +0100

    fixed get all posts

--- lambdas/getPosts.js
+++ lambdas/getPosts.js
@@ -10,14 +10,7 @@ function sortByDate (a,b){
   }
 
 const getAllPosts = async () => {
-    const response = { 
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Headers" : "Content-Type",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "*"
-        },
-     };
+    const response = {}
 
     try {
         const params = {
@@ -26,9 +19,9 @@ const getAllPosts = async () => {
         const { Items } = await dynamo.send(new ScanCommand(params));
         const data = Items.map((item) => unmarshall(item))
 
-        response.body = JSON.stringify(
-            data.sort(sortByDate)
-        );
+        response.body =  data.sort(sortByDate)
+        
+    
     } catch (e) {
         console.error(e);
         response.statusCode = 500;
@@ -39,7 +32,7 @@ const getAllPosts = async () => {
         });
     }
 
-    return data;
+    return response;
 };
 
 module.exports = { getAllPosts};
