c9048167fa85e37d38cf144b4a9080afa3ef35dd
Aminat <amibarbs01@gmail.com>
2022-08-12 03:17:38 +0100

    fixed get all posts

--- lambdas/createPost.js
+++ lambdas/createPost.js
@@ -4,27 +4,36 @@ const { uuid } = require("uuidv4");
 
 const dynamo = new DynamoDBClient({});
 
-const createPost = async (event) => {
-  const requestJSON = JSON.parse(event.body);
-  const data = {
-    postId: uuid(),
-    userId: requestJSON.userId,
-    userName: requestJSON.userName,
-    date: new Date().getTime(),
-    content: requestJSON.content,
-    comments: 0,
-  };
-  const params = {
-    TableName: process.env.DYNAMODB_TABLE_NAME,
-    Item: marshall(data),
-  };
-  const createResult = await dynamo.send(new PutItemCommand(params));
+const createPost = async (event, context) => {
+  try {
+    const requestJSON = JSON.parse(event.body);
+    const data = {
+      postId: uuid(),
+      userId: requestJSON.userId,
+      userName: requestJSON.userName,
+      date: new Date().getTime(),
+      content: requestJSON.content,
+      comments: 0,
+    };
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Item: marshall(data),
+    };
+    const createResult = await dynamo.send(new PutItemCommand(params));
 
-  const body = {
-    message: "Successfully created post.",
-    data,
-  };
-  return event;
+    const body = {
+      message: "Successfully created post.",
+      data,
+    };
+    return event;
+  } catch (err) {
+
+    const response = {
+      statusCode: 500,
+      errors: 'failed to create post',
+    }
+    context.fail(JSON.stringify(response))
+  }
 };
 
 module.exports = { createPost };
