623a88446f4247ff61797aafc5d2495164c30ece
Aminat <amibarbs01@gmail.com>
2022-08-12 03:29:15 +0100

    fixed get all posts

--- lambdas/getPostById.js
+++ lambdas/getPostById.js
@@ -1,51 +1,44 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const dynamo = new DynamoDBClient({});
-const {QueryCommand} = require("@aws-sdk/client-dynamodb");
-const {unmarshall, marshall } = require("@aws-sdk/util-dynamodb");
+const { QueryCommand } = require("@aws-sdk/client-dynamodb");
+const { unmarshall, marshall } = require("@aws-sdk/util-dynamodb");
 
-function sortByDate (a,b){
-    if(a.date >b.date){
-      return -1
-    }else return 1
+function sortByDate(a, b) {
+  if (a.date > b.date) {
+    return -1;
+  } else return 1;
+}
+
+const getPost = async (event, context) => {
+  try {
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      IndexName: "userId-postId-index",
+      ConsistentRead: false,
+      KeyConditionExpression: "userId = :userId",
+      ExpressionAttributeValues: marshall({
+        ":userId": event.path.userId,
+      }),
+    };
+
+    const { Items } = await dynamo.send(new QueryCommand(params));
+    const data = Items.map((item) => unmarshall(item));
+    const body = data.sort(sortByDate);
+    // const res = await dynamo.send(new QueryCommand(params));
+
+    // console.log(res);
+    // response.body = JSON.stringify({
+    //     message: "Successfull get request",
+    //     data: (Item) ? unmarshall(Item) : {},
+    //     rawData: Item,
+    // });
+    return body;
+  } catch (e) {
+    context.fail({
+      statusCode: 500,
+      errors: "failed to fetch users post",
+    });
   }
-
-const getPost = async (event) => {
-    
-    try {
-        const params = {
-            TableName: process.env.DYNAMODB_TABLE_NAME,
-            IndexName: "userId-postId-index",
-            ConsistentRead: false,
-            KeyConditionExpression: "userId = :userId",
-            ExpressionAttributeValues: marshall({
-                ":userId": event.path.userId
-            })
-        };
-
-        const { Items } = await dynamo.send(new QueryCommand(params));
-        const data = Items.map((item) => unmarshall(item));
-        const body = data.sort(sortByDate);
-        // const res = await dynamo.send(new QueryCommand(params));
-
-        // console.log(res);
-        // response.body = JSON.stringify({
-        //     message: "Successfull get request",
-        //     data: (Item) ? unmarshall(Item) : {},
-        //     rawData: Item,
-        // });
-        return body;
-    } catch (e) {
-        const response = {};
-        response.statusCode = 500;
-        response.body = JSON.stringify({
-            message: "Failed to get post.",
-            errorMsg: e.message,
-            errorStack: e.stack,
-        });
-        return response;
-    }
-
-    
 };
 
-module.exports = { getPost};
+module.exports = { getPost };
