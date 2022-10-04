08628b5ac3d39158636ca5d54913b11a3c988a1c
DevBurgundy <aminat@eunomy.io>
2022-10-04 01:22:26 +0100

    added cognito authorizers

--- lambdas/getPosts.js
+++ lambdas/getPosts.js
@@ -15,7 +15,8 @@ const getAllPosts = async () => {
       headers: {
           "Access-Control-Allow-Headers" : "Content-Type",
           "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "*"
+          "Access-Control-Allow-Methods": "*",
+          'Access-Control-Allow-Credentials': true,
       },
    };
 
